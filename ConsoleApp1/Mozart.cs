9d239bdac0f7f6f638bddd8fde1d56836625fe95
HFGF <HFGF@DESKTOP-3N6HI60>
2025-08-11 14:41:14 +0200

    Added Mozart as the main character in the series Avatar

--- ConsoleApp1/Mozart.cs
+++ ConsoleApp1/Mozart.cs
@@ -9,10 +9,32 @@ namespace ConsoleApp1
 {
     public class Mozart
     {
+        public static string GetCorrectInstrument()
+        {
+            string directory = Context.GetDataDirectory();
+            Console.Write("Hello dear user, please choose wisely which instrument you wise to play, 1 for clarient, 2 for flute-harp, 3 mbira or 4 for piano: ");
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    return Path.Combine(directory, "clarinet");
+                case "2":
+                    return Path.Combine(directory, "flute-harp");
+                case "3":
+                    return Path.Combine(directory, "mbira");
+                case "4":
+                    return Path.Combine(directory, "piano");
+                default:
+                    GetCorrectInstrument();
+                    break;
+            }
+
+            return null; //unreachable code, but needed to satisfy the compiler
+        }
+
+
         public static void PlaySpecificFile()
         {
-
-            string directory = Context.GetDataDirectory();
+            string directory = GetCorrectInstrument();
 
             for (int i = 0; i < 15; i++)
             {
